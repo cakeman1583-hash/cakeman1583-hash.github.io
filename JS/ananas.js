007419f54dbc3c973720003f03299097d21dd4ca
JSCodeur <138124682+JSCodeur1@users.noreply.github.com>
2025-12-15 14:47:06 +0100

    ananas sur la pizza or not ?.js

--- JS/ananas.js
+++ JS/ananas.js
@@ -1,7 +1,7 @@
 var ok=document.getElementById("voteOK");
 var or=document.getElementById("voteNeutre");
 var not=document.getElementById("voteNot");
-
+//fonctions pour actualiser le vote
 function voterOK() {
   window.localStorage.voteAnanas="OK";
   update();
