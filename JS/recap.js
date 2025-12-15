638f04d3abd24c218a891d82f0a263f3c952e1a3
JSCodeur <138124682+JSCodeur1@users.noreply.github.com>
2025-12-15 14:38:39 +0100

    Update recap.js

--- JS/recap.js
+++ JS/recap.js
@@ -1,10 +1,13 @@
 var ananas=document.getElementById("voteAnanas");
-if (window.localStorage.voteAnanas=="OK") {
-  ananas.innerHTML = "Tu as voté Pour.";
+function update() {
+  if (window.localStorage.voteAnanas=="OK") {
+    ananas.innerHTML = "Tu as voté Pour.";
+  }
+  if (window.localStorage.voteAnanas=="OR") {
+    ananas.innerHTML = "Tu as voté Neutre.";
+  }
+  if (window.localStorage.voteAnanas=="NOT") {
+    ananas.innerHTML = "Tu as voté Contre.";
+  }
 }
-if (window.localStorage.voteAnanas=="OR") {
-  ananas.innerHTML = "Tu as voté Neutre.";
-}
-if (window.localStorage.voteAnanas=="NOT") {
-  ananas.innerHTML = "Tu as voté Contre.";
-}
+update();
