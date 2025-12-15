89455e0ccb3ebb36b7fc57e230a7716d470599df
JSCodeur <138124682+JSCodeur1@users.noreply.github.com>
2025-12-15 14:34:27 +0100

    Update ananas.js

--- JS/ananas.js
+++ JS/ananas.js
@@ -2,10 +2,6 @@ var ok=document.getElementById("voteOK");
 var or=document.getElementById("voteNeutre");
 var not=document.getElementById("voteNot");
 
-if (window.localStorage.voteAnanas=="OK") {
-  window.alert("Tu as voté : Pour");
-}
-
 function voterOK() {
   window.localStorage.voteAnanas="OK";
 }
