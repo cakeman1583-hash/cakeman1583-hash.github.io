84cf67d8508a23d5a9ba4dc88706f0cfeb85f66e
JSCodeur <138124682+JSCodeur1@users.noreply.github.com>
2025-12-18 09:39:55 +0100

    ça change la couleur des bordures maintenant

--- JS/ananas.js
+++ JS/ananas.js
@@ -2,6 +2,20 @@ var ok=document.getElementById("voteOK");
 var or=document.getElementById("voteNeutre");
 var not=document.getElementById("voteNot");
 //fonctions pour actualiser le vote
+function update() {
+  if (window.localStorage.voteAnanas=="OK") {
+    ananas.innerHTML = "Tu as voté Pour.";
+    ok.style.border="1px solid gold;";
+  }
+  if (window.localStorage.voteAnanas=="OR") {
+    ananas.innerHTML = "Tu as voté Neutre.";
+    or.style.border="1px solid gold;";
+  }
+  if (window.localStorage.voteAnanas=="NOT") {
+    ananas.innerHTML = "Tu as voté Contre.";
+    not.style.border="1px solid gold;";
+  }
+}
 function voterOK() {
   window.localStorage.voteAnanas="OK";
   update();
